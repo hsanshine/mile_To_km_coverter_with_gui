075135c20c2f9f207f650d54d6a89bbee65130e1
Hamza Kyamanywa <sanshinehamza@gmail.com>
2021-01-23 23:52:40 +0900

    added files

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Mile to Km converter')
+window.minsize(width=300, height=200)
+window.config(padx=50, pady=50)
+
+
+def convert():
+    mile = float(entry.get())
+    km = round((mile * 1.609344), 2)
+    result_label.config(text=str(km))
+
+
+mile_label = tkinter.Label(text='Miles')
+mile_label.grid(column=2, row=0)
+km_label = tkinter.Label(text='km')
+km_label.grid(column=2, row=1)
+equal_label = tkinter.Label(text='is equal to')
+equal_label.grid(column=0, row=1)
+cal_button = tkinter.Button(text='Calculate', command=convert)
+cal_button.grid(column=1, row=2)
+entry = tkinter.Entry()
+entry.insert(tkinter.END, string='enter the miles')
+entry.focus()
+entry.grid(column=1, row=0)
+result_label = tkinter.Label(text='0')
+result_label.grid(column=1, row=1)
+
+window.mainloop()
